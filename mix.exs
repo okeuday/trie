d9c9ecb3a9874e4e4cc8a37e25bbdce4f28c3992
Michael Truog <mjtruog@gmail.com>
2014-12-20 13:50:40 -0800

    Version 1.4.0.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Trie.Mixfile do
 
   def project do
     [app: :trie,
-     version: "1.4.0-rc.4",
+     version: "1.4.0",
      language: :erlang,
      description: description,
      package: package,
