37391d3e705383ff513e7ebcef085b6bfefd0459
Michael Truog <mjtruog@gmail.com>
2017-03-28 18:10:12 -0700

    Version 1.7.0.

--- mix.exs
+++ mix.exs
@@ -1,9 +1,12 @@
+#-*-Mode:elixir;coding:utf-8;tab-width:2;c-basic-offset:2;indent-tabs-mode:()-*-
+# ex: set ft=elixir fenc=utf-8 sts=2 ts=2 sw=2 et nomod:
+
 defmodule Trie.Mixfile do
   use Mix.Project
 
   def project do
     [app: :trie,
-     version: "1.6.0",
+     version: "1.7.0",
      language: :erlang,
      description: description(),
      package: package(),
